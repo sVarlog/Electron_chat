71004c4ce6d9d5c6d54d68c02461445728c209c7
sVarlog <giroild@gmail.com>
2022-09-17 00:59:18 +0300

    Removed tailwind plugin, added router

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
             },
         ],
     },
-    plugins: [require("tailwindcss"), require("autoprefixer")],
+    plugins: [],
     resolve: {
         extensions: [".js"],
     },
