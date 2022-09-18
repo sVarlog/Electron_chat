37300cc1a16197ec11457a5d5af7273dd7af1a00
sVarlog <giroild@gmail.com>
2022-09-19 00:26:23 +0300

    Added creating user profiles

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,13 +1,25 @@
+import { db } from "../db/firestore";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const registerUser = async ({ email, password }) => {
+const createUserProfile = async (userProfile) => {
+    await db.collection("profiles").doc(userProfile.uid).set(userProfile);
+};
+
+export const registerUser = async ({ email, password, username, avatar }) => {
     try {
         const { user } = await firebase
             .auth()
             .createUserWithEmailAndPassword(email.trim(), password);
-        return user;
+        await createUserProfile({
+            uid: user.uid,
+            username,
+            email,
+            avatar,
+            joinedChats: [],
+        });
     } catch (e) {
+        console.log("error", e);
         return Promise.reject(e.messsage);
     }
 };
