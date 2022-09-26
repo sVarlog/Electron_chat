17c7c4f632b3c1b3fb635797ef5c717dbf9f0224
sVarlog <giroild@gmail.com>
2022-09-26 14:08:28 +0300

    Added preloaders to chat page

--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -6,6 +6,8 @@ import { ViewTitle } from "../components/shared/ViewTitle";
 import { BaseLayout } from "../layouts/Base";
 import { useDispatch, useSelector } from "react-redux";
 import { subscribeToChat, subscribeToProfile } from "../actions/chats";
+import { LoadingView } from "../components/shared/LoadingView";
+import { Loader } from "../components/shared/Loader";
 
 export const Chat = () => {
     const { id } = useParams();
@@ -46,13 +48,34 @@ export const Chat = () => {
     return (
         <BaseLayout canGoBack componentName={Chat.name}>
             <div className="row no-gutters fh">
-                <div className="col-3 fh">
-                    <ChatUsersList users={activeChat?.joinedUsers} />
+                <div
+                    className={`col-3 fh ${
+                        !activeChat?.id && "d-flex justify-content-center pt-5"
+                    }`}
+                >
+                    {!activeChat?.id ? (
+                        <Loader />
+                    ) : (
+                        <ChatUsersList users={activeChat?.joinedUsers} />
+                    )}
                 </div>
 
-                <div className="col-9 fh">
-                    <ViewTitle text={`Channel: ${activeChat?.name || ""}`} />
-                    <ChatMessagesList />
+                <div
+                    className={`col-9 fh ${
+                        !activeChat?.id &&
+                        "d-flex align-items-center justify-content-center"
+                    }`}
+                >
+                    {!activeChat?.id ? (
+                        <Loader />
+                    ) : (
+                        <>
+                            <ViewTitle
+                                text={`Channel: ${activeChat?.name || ""}`}
+                            />
+                            <ChatMessagesList />
+                        </>
+                    )}
                 </div>
             </div>
         </BaseLayout>
